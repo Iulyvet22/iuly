455ed41a5347ac5c6d94257f0a48a183829b9b0a
Iulyvet22 <127765054+Iulyvet22@users.noreply.github.com>
2023-03-15 16:50:59 +0100

    Add files via upload

---
+++ triangolo.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    int y;
+    int z;
+    printf("inserisci lato x");
+    scanf("%d", &x);
+    printf("inserisci lato y");
+    scanf("%d", &y);
+    printf("inserisci lato z");
+    scanf("%d", &z);
+    if((x+y)>z && (y+z)>x && (x+z)>y)
+    {
+        printf("può essere un triangolo\n");
+    }
+  
+    else
+    {
+        printf("non può essere un triangolo\n");
+    }
+    printf("inserisci i lati\n");
+    scan("%d %d %d", &x , &y , &z)
+    if(x+y>z&&y+z>x&&x+z>y)
+    {
+    if(x==y||y==z|x==z)
+    {
+     printf("è equilatero\n");
+    }
+    else if (x==y||y==z||X==Z)
+    {
+    
+    }
+    }
+}
